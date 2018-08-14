3b4d039dc51e3ff7afa190afd87bbed9f3824e8e
Yury Shashkin <yshashkin@yandex.ru>
2018-08-15 00:54:08 +0300

    2222

--- db/migrate/20180812160749_remove_parts_from_kontragents.rb
+++ db/migrate/20180812160749_remove_parts_from_kontragents.rb
@@ -1,14 +1,4 @@
 class RemovePartsFromKontragents < ActiveRecord::Migration[5.2]
   def change
-    remove_column :kontragents, :number, :integer
-    remove_column :kontragents, :type, :string
-    remove_column :kontragents, :full_name, :string
-    remove_column :kontragents, :group, :string
-    remove_column :kontragents, :INN, :string
-    remove_column :kontragents, :KPP, :string
-    remove_column :kontragents, :kod, :string
-    remove_column :kontragents, :UAddress, :string
-    remove_column :kontragents, :FAddress, :string
-    remove_column :kontragents, :otherInf, :text
   end
 end
